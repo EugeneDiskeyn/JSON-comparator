7fb155e17c2119ad5e7ef72da055b13ba535fcb0
Eugene <jdorovko@gmail.com>
2024-04-04 00:51:21 +0300

    almost finished step 6

--- src/comparison.js
+++ src/comparison.js
@@ -1,62 +1,126 @@
 export const showComparation = (file1, file2) => {
-    const comparedObject = getSortedFile(getComparedObjectWithSigns(file1, file2));
-    return getComparedFileAsString(comparedObject);
+    const comparedFile = getComparedObjectWithSigns(file1, file2);
+    console.log(getComparedFileAsString(comparedFile));
+    // return getComparedFileAsString(comparedFile);
+}
+
+
+const getComparedFileAsString = (comparedFile, tabulation = "") => {
+    const array = Object.keys(comparedFile).map((key) => {
+        if (typeof(comparedFile[key][1]) !== "object") { 
+            return `${tabulation}${comparedFile[key][0]} "${key}": "${comparedFile[key][1]}"`;
+        } else {
+            return `${tabulation}${comparedFile[key][0]} "${key}":\n${getComparedFileAsString(comparedFile[key][1], tabulation + "    ")}`;
+        }
+    })
+
+    return array.join("\n");
 }
 
 
 const getComparedObjectWithSigns = (file1, file2) => {
-    const keys1 = Object.keys(file1);
+
+    // const comparedFile = firstLoop(file2);
+    const comparedFile = thirdLoop(file1, file2)
+    return comparedFile;
+}
+
+
+const firstLoop = (file2) => {
+
     const keys2 = Object.keys(file2);
-    let comparedObject = {};
+
+    const comparedFile = {};
+
+    for (let key of keys2) {
+
+        comparedFile[key] = [];
+        comparedFile[key][0] = "*";
+        comparedFile[key][1] = {};
+
+        if (typeof(file2[key]) !== "object") {
+            comparedFile[key][1] = file2[key];
+        } else {
+            comparedFile[key][1] = firstLoop(file2[key]);
+        }
+    }
+    return comparedFile;
+} 
+
+
+const thirdLoop = (file1, file2) => {
+
+    const keys1 = file1 === undefined? [] : Object.keys(file1);
+    const keys2 = file2 === undefined? [] : Object.keys(file2);
+
+    const keys = getAllKeys(keys1, keys2).sort();
+
+    const comparedFile = {}
+    
+    for (let key of keys) {
+        comparedFile[key] = [];
+        if (file1[key] !== undefined && file2[key] !== undefined) {
+            if (typeof(file1[key]) === "object" && typeof(file2[key]) === "object") {
+                comparedFile[key][0] = " ";
+                comparedFile[key][1] = thirdLoop(file1[key], file2[key]);
+            } else if (typeof(file1[key]) === "object" && typeof(file2[key]) !== "object") {
+                comparedFile[key][0] = "+";
+                comparedFile[key][1] = file2[key];
+            } else if (typeof(file1[key]) !== "object" && typeof(file2[key]) === "object") {
+                comparedFile[key][0] = "+";
+                comparedFile[key][1] = file2[key];
+            } else {
+                if (file1[key] === file2[key]) {
+                    comparedFile[key][0] = " ";
+                    comparedFile[key][1] = file2[key];
+                } else {
+                    comparedFile[key][0] = "+";
+                    comparedFile[key][1] = file2[key];
+                }
+            }
+        } else if (file1[key] !== undefined && file2[key] === undefined) {
+            if (typeof(file1[key]) === "object") {
+                comparedFile[key][0] = "-";
+                comparedFile[key][1] = thirdLoop(file1[key], {}); 
+            } else {
+                comparedFile[key][0] = "-";
+                comparedFile[key][1] = file1[key]; 
+            }
+        } else {
+            comparedFile[key] = [];
+            comparedFile[key][0] = "*";
+            comparedFile[key][1] = {};
+    
+            if (typeof(file2[key]) !== "object") {
+                comparedFile[key][1] = file2[key];
+            } else {
+                comparedFile[key][1] = thirdLoop({}, file2[key]);
+            }
+        }
+    }
+    return comparedFile;
+}
+
+
+const getAllKeys = (keys1, keys2) => {
+
+    let keys = [];
+    let flag;
+
+    for (let key1 of keys1) {
+        keys.push(key1);
+    }
 
     for (let key2 of keys2) {
-        comparedObject[key2] = [];
-        comparedObject[key2][0] = "*";
-        comparedObject[key2][1] = file2[key2];
-    }
-    
-    for (let key1 of keys1) {
-        const key2 = keys2.find((key2) => {
-            if (key1 === key2) {
-                return key2;
-            } else {
-                return false;
+        flag = true;
+        for (let key of keys) {
+            if (key2 === key) {
+                flag = false;
             }
-        })
-
-        comparedObject[key1] = [];
-
-        if (key2 && file1[key1] === file2[key2]) {
-            comparedObject[key1][0] = " ";
-            delete file2[key1];
-        } else if (key2) {
-            comparedObject[key1][0] = "+";
-            delete file2[key1];
-        } else {
-            comparedObject[key1][0] = "-";
         }
-        comparedObject[key1][1] = file1[key1];
+        if (flag === true) {
+            keys.push(key2);
+        }
     }
-
-    return comparedObject;
-}
-
-
-const getSortedFile = (file) => {
-    const sortedKeys = Object.keys(file).sort();
-    let sortedFile = {};
-
-    for (let key of sortedKeys) {
-        sortedFile[key] = file[key];
-    }
-    
-    return sortedFile;
-}
-
-const getComparedFileAsString = (comparedObject) => {
-    const array = Object.keys(comparedObject).map((element) => {
-        return`${comparedObject[element][0]} "${element}": "${comparedObject[element][1]}"`;
-    })
-
-    return array.join("\n");
+    return keys;
 }
